056babfbe3389bebee4b0ea7ec31f74a6983aa71
Roman Kohutyak <romankohutyak@gmail.com>
2020-09-12 14:45:54 +0300

    feat: implement extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,51 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let bigStr = '';
+  let smallStr = '';
+
+  if (options.repeatTimes === undefined) {
+    options.repeatTimes = 1;
+  }
+  if (options.additionRepeatTimes === undefined) {
+    options.additionRepeatTimes = 1;
+  }
+  if (options.separator === undefined) {
+    options.separator = '+';
+  }
+
+  // How to get out from if Hell? T_T
+  if (options.addition !== undefined) {
+    if (options.additionSeparator !== undefined) {
+      if (options.additionRepeatTimes !== undefined) {
+        for (let i = 0; i < options.additionRepeatTimes; i++) {
+          smallStr += options.addition + options.additionSeparator;
+        }
+      } else {
+        smallStr += options.addition + options.additionSeparator;
+      }
+      smallStr = smallStr.slice(0, -options.additionSeparator.length);
+    } else if (options.additionRepeatTimes !== undefined) {
+      for (let i = 0; i < options.additionRepeatTimes; i++) {
+        smallStr += options.addition;
+      }
+    } else {
+      smallStr = options.addition;
+    }
+  }
+
+  if (options.repeatTimes !== undefined) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      if (options.separator !== undefined) {
+        bigStr += str + smallStr + options.separator;
+      } else {
+        bigStr += str + smallStr;
+      }
+    }
+    if (options.separator !== undefined) {
+      bigStr = bigStr.slice(0, -options.separator.length);
+    }
+  }
+
+  return bigStr;
 };
-  
