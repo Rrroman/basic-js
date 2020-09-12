2a80483ce0233d19a6cf27f1416d2510e2475762
Roman Kohutyak <romankohutyak@gmail.com>
2020-09-12 15:04:02 +0300

    feat: implement dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,22 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  // throw new CustomError('Not implemented');
+
+  let result = '';
+  if (members === null || members === undefined || members.length === 0) {
+    return false;
+  }
+
+  for (let i = 0; i < members.length; i++) {
+    let element = members[i];
+
+    if (typeof element === 'string' && element.length > 0) {
+      char = element.trim().toUpperCase()[0];
+      result += char;
+    }
+  }
+
+  result = result.split('').sort().join('');
+  return result;
 };
