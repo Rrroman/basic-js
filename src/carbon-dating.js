ea1213e316bd5af099e2dc0d213ac926b1a6551f
Roman Kohutyak <romankohutyak@gmail.com>
2020-10-11 00:40:36 +0300

    feat: implement carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,16 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const naturalLogarithmOfTwo = 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') return false;
+  sampleActivity = parseFloat(sampleActivity);
+  if (isNaN(sampleActivity) || sampleActivity <= 0 || sampleActivity > 15) {
+    return false;
+  }
+
+  const rateConstant = naturalLogarithmOfTwo / HALF_LIFE_PERIOD;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / rateConstant);
 };
