410c0c6395665c883005a7d34d8ee892a2f2dda8
Roman Kohutyak <romankohutyak@gmail.com>
2020-10-12 21:25:19 +0300

    feat: implement basic encrypt and decrypt

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,135 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+    this.arrAbs = [
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h',
+      'i',
+      'j',
+      'k',
+      'l',
+      'm',
+      'n',
+      'o',
+      'p',
+      'q',
+      'r',
+      's',
+      't',
+      'u',
+      'v',
+      'w',
+      'x',
+      'y',
+      'z',
+    ];
+    this.max = 25;
+  }
+  encrypt(str, code) {
+    if (
+      typeof str !== 'string' ||
+      typeof code !== 'string' ||
+      str === '' ||
+      code === ''
+    ) {
+      throw new Error('Add valid parameters');
+    }
+    let sum = null;
+    str = str.toLowerCase();
+    code = code.toLowerCase();
+    let arrCode = [];
+    let arrCipher = [];
+    let regex = /[a-zA-Z]/;
+    while (str.length > arrCode.length) {
+      for (let i = 0, j = 0; i < code.length, j < str.length; i++, j++) {
+        if (code.length === i) {
+          i = 0;
+        }
+        const element = code[i];
+        if (!str[j].match(regex)) {
+          arrCode.push(str[j]);
+          i--;
+        } else {
+          arrCode.push(element);
+        }
+      }
+    }
+    for (let i = 0, j = 0; i < str.length, j < arrCode.length; i++, j++) {
+      const strElement = str[i];
+      const codeElement = arrCode[i];
+      const indexOfStrCharacter = this.arrAbs.indexOf(strElement);
+      const indexOfCodeCharacter = this.arrAbs.indexOf(codeElement);
+      sum = indexOfStrCharacter + indexOfCodeCharacter;
+      if (indexOfStrCharacter === -1) {
+        arrCipher.push(str[i]);
+      } else if (sum < this.max) {
+        arrCipher.push(this.arrAbs[sum]);
+      } else {
+        sum = sum - this.max - 1;
+        arrCipher.push(this.arrAbs[sum]);
+      }
+    }
+    return this.direct
+      ? arrCipher.join('').toUpperCase()
+      : arrCipher.reverse().join('').toUpperCase();
+  }
+
+  decrypt(str, code) {
+    if (
+      typeof str === 'undefined' ||
+      typeof code === 'undefined' ||
+      str === '' ||
+      code === ''
+    ) {
+      throw new Error('Add valid parameters');
+    }
+
+    str = str.toLowerCase();
+    code = code.toLowerCase();
+    let arrCode = [];
+    let arrCipher = [];
+    let regex = /[a-zA-Z]/;
+    while (str.length > arrCode.length) {
+      for (let i = 0, j = 0; i < code.length, j < str.length; i++, j++) {
+        if (code.length === i) {
+          i = 0;
+        }
+        const element = code[i];
+        if (!str[j].match(regex)) {
+          arrCode.push(str[j]);
+          i--;
+        } else {
+          arrCode.push(element);
+        }
+      }
+    }
+
+    for (let i = 0, j = 0; i < str.length, j < arrCode.length; i++, j++) {
+      const strElement = str[i];
+      const codeElement = arrCode[i];
+      const indexOfStrCharacter = this.arrAbs.indexOf(strElement);
+      const indexOfCodeCharacter = this.arrAbs.indexOf(codeElement);
+      let subtraction = indexOfStrCharacter - indexOfCodeCharacter;
+      if (indexOfStrCharacter === -1) {
+        arrCipher.push(str[i]);
+      } else if (subtraction >= 0) {
+        arrCipher.push(this.arrAbs[subtraction]);
+      } else {
+        subtraction = this.max + subtraction + 1;
+        arrCipher.push(this.arrAbs[subtraction]);
+      }
+    }
+    return this.direct
+      ? arrCipher.join('').toUpperCase()
+      : arrCipher.reverse().join('').toUpperCase();
   }
 }
 
